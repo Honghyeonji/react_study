b0d758d8dcaab6bef188ce9981c28dd934fead79
SuperH0ng <81635179+SuperH0ng@users.noreply.github.com>
2021-11-02 16:38:01 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import PersonalityTest from './personalityTest'
+import TestName from './testName'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <TestName name="심리테스트"/>
+        <PersonalityTest />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
