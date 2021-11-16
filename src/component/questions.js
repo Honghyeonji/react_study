73f21262288ef6b81d82c4acac9057bda111aa0c
SuperH0ng <ghdtmdgus100@gmail.com>
2021-11-16 15:18:12 +0900

    update...

--- src/component/questions.js
+++ src/component/questions.js
@@ -1,7 +1,14 @@
 export const questions = [
-    {
+    {   
+        question_number : 1,
         question : "1. 당신은 언제 가장 기분이 좋습니까?",
         answer: ["아침", "오후부터 이른 저녁까지", "야심한 밤"],
         answer_value: [2, 4, 6]
+    },
+    {
+        question_number : 2,
+        question : "2.당신의 평소 걸음걸이는 어떠합니까?",
+        answer: ["보폭이 크고,빠른 걸음", "빠른걸음이나 보폭이 작은 걸음", "빠르지 않고,고개를 들고 주위를 둘러보는 걸음", "매우 느린 걸음"],
+        answer_value: [6, 4, 7, 2]
     }
 ];
